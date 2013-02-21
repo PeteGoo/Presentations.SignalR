0813e4605c99a6468e04246169503188264fff34
petegoo <pete@petegoo.com>
2013-02-21 23:19:03 +1300

    Added move shape demo

---
+++ SignalR.Sample/MoveShapeDemo/MoveShape.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalR.Sample.MoveShapeDemo {
+    [HubName("moveShape")]
+    public class MoveShapeHub : Hub {
+        public void MoveShape(int x, int y) {
+            Clients.AllExcept(Context.ConnectionId).shapeMoved(x, y);
+        }
+    }
+}
