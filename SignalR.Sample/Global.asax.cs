1ebcecfb63948173cd90ef39f1785863a1b097d9
Peter Goodman <github@petegoo.com>
2013-02-27 00:13:03 +1300

    Cleaned up demos and added beginning of todo sample

---
+++ SignalR.Sample/Global.asax.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Routing;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+
+namespace SignalR.Sample {
+    public class Global : System.Web.HttpApplication {
+
+        protected void Application_Start(object sender, EventArgs e) {
+            RouteTable.Routes.MapHttpRoute(
+                   name: "API Default",
+                   routeTemplate: "api/{controller}/{id}",
+                   defaults: new { id = RouteParameter.Optional }
+               );
+            var jsonFormatter = GlobalConfiguration.Configuration.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+
+        protected void Session_Start(object sender, EventArgs e) {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e) {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e) {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e) {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e) {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e) {
+
+        }
+    }
+}
---
+++ SignalR.Sample/SharedTodos/TodoController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SignalR.Sample.SharedTodos {
+    public class TodoController : ApiController {
+        private static readonly ConcurrentDictionary<string, Todo> todos = new ConcurrentDictionary<string, Todo>();
+
+        public IEnumerable<Todo> Get() {
+            return todos.Values;
+        }
+
+        public void Put(Todo todo) {
+            todos.AddOrUpdate(todo.Description, i => todo, (i, todo1) => todo);
+        }
+
+        public void Post(Todo todo) {
+            todos.AddOrUpdate(todo.Description, i => todo, (i, todo1) => todo);
+        }
+    }
+
+
+    public class Todo {
+        public string Description { get; set; }
+        public bool Done { get; set; }
+    }
+}
